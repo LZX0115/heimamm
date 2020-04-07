bf4c8bc0adb85fe1acacc0df45dd16b2e209348b
DESKTOP-AJ70BPQ\BCD115 <you@example.com>
2020-04-07 22:59:48 +0800

    login标题

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -1,5 +1,5 @@
 <template>
-   <!-- 最外层标签的class名字使用组件名     -->
+  <!-- 最外层标签的class名字使用组件名     -->
   <div class="login">
     <div class="left">
       <div class="title">
@@ -8,26 +8,22 @@
         <span class="titleLine">|</span>
         <span class="titleName2">用户登陆</span>
       </div>
-
-    
-           <!-- 栅栏处理共24栏
-             el-row：行
-                它有一个子项是el-col 代表栏 多少栏  :span="栏数"
-        -->
-        <div class="right">
+    </div>
+    <div class="right">
       <img src="@/assets/img/login_banner_ele.png" alt />
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "login"
+};
 </script>
 
 <style lang="less">
 .login {
-    // 弹性盒子布局
+  // 弹性盒子布局
   display: flex;
   //   主轴布局
   justify-content: space-around;
@@ -39,29 +35,26 @@ export default {
     rgba(20, 147, 250, 1),
     rgba(1, 198, 250, 1)
   );
-  .left{
-      width: 478px;
+  .left {
+    width: 478px;
     height: 550px;
     background: rgba(245, 245, 245, 1);
     padding: 43px;
-    .title {
-      margin-bottom: 20px;
-      .titleName {
-        font-size: 24px;
-        font-weight: 400;
-        color: rgba(12, 12, 12, 1);
-        margin-left: 15px;
-        margin-right: 15px;
-      }
-      .titleLine {
-        color: #ccc;
-        font-size: 24px;
-      }
-       .titleName2 {
-        font-size: 22px;
-        margin-left: 15px;
-      }
+    .titleName {
+      font-size: 24px;
+      font-weight: 400;
+      color: rgba(12, 12, 12, 1);
+      margin-left: 15px;
+      margin-right: 15px;
+    }
+    .titleLine {
+      color: #ccc;
+      font-size: 24px;
+    }
+    .titleName2 {
+      font-size: 22px;
+      margin-left: 15px;
+    }
   }
-}
 }
 </style>
